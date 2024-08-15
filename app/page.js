a5fef96a0d3099054dcd4e0f0def59f7036b41c5
James O'Kane <orbiccode@gmail.com>
2024-08-15 15:24:24 +0100

    day 4 complete

--- app/page.js
+++ app/page.js
@@ -6,7 +6,6 @@ import Main from "/src/componets/Main/Main.jsx"
 export default function Home() {
   return (
     <>
-      <Link href="/founders">Founders</Link>
       <Main />
     </>
     
--- src/componets/Header/Header.jsx
+++ src/componets/Header/Header.jsx
@@ -1,11 +1,12 @@
 "use client"
-
+import Link from "next/link"
 import styles from './Header.module.css'
 import { useState } from 'react'
 
 
 const Header = (props) => {
-  const[isMenuOpen, setIsMenuOpen] = useState(false);
+  
+const[isMenuOpen, setIsMenuOpen] = useState(false);
 
 function handleClick() {
   setIsMenuOpen(!isMenuOpen)
@@ -14,13 +15,17 @@ function handleClick() {
   return (
     <header className={styles.headerContainer}>
       <h1 className={styles.title}>{props.title} Limgrave Palace</h1>
-      <button onClick={() => handleClick()}>Menu Toggle</button>
-      {
-        isMenuOpen && (
+      <img className={styles.img} src="/menu-open-button.png" onClick={() => handleClick()} />
+      { 
+      isMenuOpen && (
           <>
-          
-
-            <button onClick={handleClick}>See the partners</button>
+            <div className={styles.menu}>
+              <img src="/menu-close-button.png" />
+                <div className={styles.link}>
+              <Link href="/app">Home</Link>
+              <Link href="/founders">Founders</Link>
+                </div>
+            </div>
           </>
         )
       }
@@ -30,3 +35,7 @@ function handleClick() {
 };
 
 export default Header;
+
+
+// make div for orange menu
+// add links to the menu div
