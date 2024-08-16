2a712018fbf6c15a5658fabb0fbf5838ae965732
dtron500 <daniel.mitchell3005@gmail.com>
2024-08-16 10:06:20 +0100

    ENGLAND

--- src/componets/Main/Main.jsx
+++ src/componets/Main/Main.jsx
@@ -1,11 +1,12 @@
 import HeroSection from "../Hero/HeroSection/HeroSection"
 import Separator from "../Separator/Separator";
 import HowItWorksSection from "../HowItWorks/HowItWorksSection/HowItWorksSection";
-
+import Reviews from "../Reviews/Reviews.jsx";
 const Main = () => {
   return (
     <main>
       <HeroSection heroImagesrc="/hero-desktop.png" />
+      <Reviews />
       <Separator />
       <HowItWorksSection />
     </main>
--- src/componets/Reviews/Reviews.jsx
+++ src/componets/Reviews/Reviews.jsx
@@ -1,17 +1,25 @@
+"use client"
 import styles from "./Reviews.module.css"
 
 export default function Reviews () {
+function handleClick(country) {
+    console.log(country)
+}
     return (
         <>
             <div className={styles.reviewBox}>
                 <header className={styles.header}></header>
                 <p className={styles.description}></p>
-                <div>
-                    <button className={styles.englandButton}></button>
-                    <button></button>
-                    <button></button>
+                <div className={styles.buttonSection}>
+                    <button className={styles.englandButton} onClick={() => handleClick('England')}>England</button>
+                    <button className= {styles.walesButton}>Wales</button>
+                    <button className={styles.scotlandButton}>Scotland</button>
                 </div>
+                <div className={styles.reviewData}></div>
             </div>
         </>
     )
 }
+
+// Add on click events to each button logging a test to show it worked
+//
