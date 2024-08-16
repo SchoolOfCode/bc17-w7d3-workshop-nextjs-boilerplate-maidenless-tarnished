eac0f36d28f06f3a59e8dfa3216d0324a801195d
Eric-0805 <ericroberts805@gmail.com>
2024-08-16 11:44:57 +0100

    fetch not work

--- src/componets/Reviews/Reviews.jsx
+++ src/componets/Reviews/Reviews.jsx
@@ -1,19 +1,37 @@
 "use client"
 import styles from "./Reviews.module.css"
+import { useState, useEffect } from 'react';
 
 export default function Reviews () {
-function handleClick(country) {
-    console.log(country)
+
+    const [reviews, setReviews] = useState('');
+    console.log(reviews);
+
+    useEffect(() => {
+
+        if (reviews) {
+            fetch(`https://seal-app-336e8.ondigitalocean.app/reviews?country=scotland`)
+                .then(response => response.json())
+                .then(data => setReviews(data));
+        }
+
+    }, [])
+
+
+function setCountry(country) {
+    setReviews(country);
 }
+
     return (
         <>
             <div className={styles.reviewBox}>
                 <header className={styles.header}></header>
                 <p className={styles.description}></p>
+
                 <div className={styles.buttonSection}>
-                    <button className={styles.englandButton} onClick={() => handleClick('England')}>England</button>
-                    <button className= {styles.walesButton}>Wales</button>
-                    <button className={styles.scotlandButton}>Scotland</button>
+                    <button className={styles.englandButton} onClick={() => setCountry('England')}>England</button>
+                    <button className= {styles.walesButton} onClick={() => setCountry('Wales')}>Wales</button>
+                    <button className={styles.scotlandButton} onClick={() => setCountry('Scotland')}>Scotland</button>
                 </div>
                 <div className={styles.reviewData}></div>
             </div>
