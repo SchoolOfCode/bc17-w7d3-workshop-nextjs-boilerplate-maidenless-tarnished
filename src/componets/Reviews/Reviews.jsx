e061e2865b94afab884ad434a8c5a75cb8cf2062
James O'Kane <orbiccode@gmail.com>
2024-08-22 20:29:00 +0100

    review section done

--- src/componets/Reviews/Reviews.jsx
+++ src/componets/Reviews/Reviews.jsx
@@ -22,16 +22,16 @@ export default function Reviews () {
         <>
             <div className={styles.reviewBox}>
                 <header className={styles.header}>Trusted.</header>
-                <p className={styles.description}>We've got thousands of happy customers all over the UK. Choose your countery to see the latest review:</p>
+                <p className={styles.description}>We've got thousands of happy customers all over the UK. Choose your country to see the latest review:</p>
 
                 <div className={styles.buttonSection}>
-                    <button className={styles.englandButton} onClick={() => setCountry('england')}>England</button>
-                    <button className={styles.walesButton} onClick={() => setCountry('wales')}>Wales</button>
-                    <button className={styles.scotlandButton} onClick={() => setCountry('scotland')}>Scotland</button>
+                    <button className={styles.button} onClick={() => setCountry('england')}>England</button>
+                    <button className={styles.button} onClick={() => setCountry('wales')}>Wales</button>
+                    <button className={styles.button} onClick={() => setCountry('scotland')}>Scotland</button>
                 </div>
                 <div className={styles.reviewData}>
-                    <p>{reviews.text}</p>
-                    <p>{reviews.author}</p>
+                    <p className={styles.reviewText}>{reviews.text}</p>
+                    <p className={styles.reviewAuthor}>{reviews.author}</p>
                 </div>
             </div>
         </>
